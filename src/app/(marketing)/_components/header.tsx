8fa807e224e8b7919f02a8559154fffc951e32b5
João Gabriel <joaogabriel9633@gmail.com>
2025-11-05 18:28:46 -0300

    hero landing page

--- src/app/(marketing)/_components/header.tsx
+++ src/app/(marketing)/_components/header.tsx
@@ -1,3 +1,9 @@
 export function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="fixed w-full z-99999 px-4 text-white">
+      <div className="w-full max-w-6xl mx-auto flex items-center justify-between">
+        Header
+      </div>
+    </header>
+  );
 }
--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,4 +1,7 @@
+import { ShootingStars } from "@/components/ui/shooting-stars";
+import { StarsBackground } from "@/components/ui/stars-background";
 import React from "react";
+import { Header } from "./_components/header";
 
 export default function MarketingLayout({
   children,
@@ -6,8 +9,13 @@ export default function MarketingLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="h-full">
-      <main>{children}</main>
+    <div className="h-full relative bg-[#09090b]">
+      <Header />
+      <main className="relative h-full flex items-center justify-center z-99999">
+        {children}
+      </main>
+      <ShootingStars />
+      <StarsBackground />
     </div>
   );
 }
