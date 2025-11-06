240aabff3e3b1e64d33d4a777efa01af60e03a6c
João Gabriel <joaogabriel9633@gmail.com>
2025-11-06 13:59:04 -0300

    update nav user

--- src/app/(routes)/(tracking)/_components/sidebar/nav-user.tsx
+++ src/app/(routes)/(tracking)/_components/sidebar/nav-user.tsx
@@ -25,6 +25,8 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 export function NavUser({
   user,
@@ -36,6 +38,28 @@ export function NavUser({
   };
 }) {
   const { isMobile } = useSidebar();
+  const { data: session, isPending } = authClient.useSession();
+  const router = useRouter();
+
+  function getInitials(name: string): string {
+    const initials = name
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase())
+      .slice(0, 2)
+      .join("");
+
+    return initials;
+  }
+
+  const hanldeLogout = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onRequest: () => {
+          router.push("/sign-in");
+        },
+      },
+    });
+  };
 
   return (
     <SidebarMenu>
@@ -47,12 +71,28 @@ export function NavUser({
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                {session?.user.image && (
+                  <AvatarImage
+                    src={session.user.image}
+                    alt={session.user.name}
+                  />
+                )}
+                {session?.user.name && (
+                  <AvatarFallback className="rounded-lg">
+                    {" "}
+                    {getInitials(session.user.name)}{" "}
+                  </AvatarFallback>
+                )}
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
+                {session?.user.name && (
+                  <span className="truncate font-medium">
+                    {session?.user.name}
+                  </span>
+                )}
+                {session?.user.email && (
+                  <span className="truncate text-xs">{session.user.email}</span>
+                )}
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -66,12 +106,29 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  {session?.user.image && (
+                    <AvatarImage
+                      src={session?.user.image}
+                      alt={session?.user.name}
+                    />
+                  )}
+                  {session?.user.name && (
+                    <AvatarFallback className="rounded-lg">
+                      {getInitials(session?.user.name)}
+                    </AvatarFallback>
+                  )}
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
+                  {session?.user.name && (
+                    <span className="truncate font-medium">
+                      {session?.user.name}
+                    </span>
+                  )}
+                  {session?.user.email && (
+                    <span className="truncate text-xs">
+                      {session.user.email}
+                    </span>
+                  )}
                 </div>
               </div>
             </DropdownMenuLabel>
@@ -98,7 +155,7 @@ export function NavUser({
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>
+            <DropdownMenuItem onClick={hanldeLogout} className="cursor-pointer">
               <LogOut />
               Log out
             </DropdownMenuItem>
