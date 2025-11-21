42a32997aba76cfe321ec2fed3d6d212bafd7fb4
João Gabriel <joaogabriel9633@gmail.com>
2025-11-21 18:14:18 -0300

    feat: add lead routing index to consolidate lead-related route handlers.

--- src/app/router/leads/index.ts
+++ src/app/router/leads/index.ts
@@ -5,8 +5,10 @@ import { deleteLead } from "./delete";
 import { addLeadFirst } from "./add-lead-to-first";
 import { addLeadLast } from "./add-lead-to-last";
 import { updateLeadOrder } from "./update-order";
+import { getLead } from "./get";
 
 export const leadRoutes = {
+  get: getLead,
   search: searchLeads,
   create: createLead,
   update: updateLead,
