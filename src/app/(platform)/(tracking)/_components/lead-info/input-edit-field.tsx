2c8661b518d3720d3815855f305d4fc9d9d98706
Arthur Fabrícyo <arthur.fabricyo@gmail.com>
2026-01-19 18:02:18 -0300

    feat/finalizado o lado esquerdo dos detalhes do lead

---
+++ src/app/(platform)/(tracking)/_components/lead-info/input-edit-field.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { TypeFieldLead } from "./Info-item";
+import { Input } from "@/components/ui/input";
+import { phoneMask } from "@/utils/format-phone";
+
+export interface EditingInputComponentProps {
+  value: string;
+  onSubmit: (value: string) => void;
+  type: TypeFieldLead;
+}
+
+export const InputEditField = ({
+  value,
+  onSubmit,
+  type,
+}: EditingInputComponentProps) => {
+  const [localValue, setLocalValue] = useState(value);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    onSubmit(localValue);
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Input
+        className="h-8 text-xs"
+        autoFocus
+        value={type === "phone" ? phoneMask(localValue) : localValue}
+        onChange={(e) => setLocalValue(e.target.value)}
+        onBlur={handleSubmit}
+      />
+      <button type="submit" className="hidden sr-only" />
+    </form>
+  );
+};
---
+++ src/app/(platform)/(tracking)/_components/lead-info/select-edit-field.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { EditingInputComponentProps } from "./input-edit-field"; 
+import { useListTrackingParticipants } from "@/features/users/use-list-tracking-participants";
+import { useState } from "react";
+
+interface EditingDropdownComponentProps extends EditingInputComponentProps {
+  label?: string;
+  trackingId: string;
+}
+
+export const SelectEditField = ({
+  value,
+  onSubmit,
+  trackingId,
+}: EditingDropdownComponentProps) => {
+
+const {data} = useListTrackingParticipants(trackingId)
+
+  const userSelectable = data ? data.participants.flatMap((p) => p.user) : [];
+
+  const handleChange = (newValue: string) => {
+    // Convert ID to name
+    const selectedUser = userSelectable.find((user) => user.id === newValue);
+    if (selectedUser) {
+      onSubmit(selectedUser.id);
+    }
+  };
+
+  const labelSelect = userSelectable.find((user) => user.id === value)?.name ?? "Selecione";
+
+  return (
+    <Select value={value} onValueChange={handleChange}>
+      <SelectTrigger className="w-45" size="sm" autoFocus>
+        <SelectValue
+        >{labelSelect}</SelectValue>
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {userSelectable.map((participant) => (
+            <SelectItem
+              key={`user-selectable-${participant.id}`}
+              value={participant.id}
+            >
+              {participant.name}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
