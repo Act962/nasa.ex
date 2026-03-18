201c70decc6f67c1d629a4c2f9f6cb6e2921df51
João Gabriel <joaogabriel9633@gmail.com>
2026-03-18 10:47:10 -0300

    init

--- src/features/agenda/components/external-link/booking-form.tsx
+++ src/features/agenda/components/external-link/booking-form.tsx
@@ -2,6 +2,9 @@
 
 import { Card, CardContent } from "@/components/ui/card";
 import { useQueryPublicAgenda } from "../../hooks/use-public-agenda";
+import { Calendar, Clock } from "lucide-react";
+import dayjs from "dayjs";
+import { Separator } from "@/components/ui/separator";
 
 interface BookingFormProps {
   orgSlug: string;
@@ -13,7 +16,7 @@ export function BookingForm({ orgSlug, agendaSlug }: BookingFormProps) {
 
   return (
     <Card className="max-w-250 w-full mx-auto">
-      <CardContent className="p-5 md:grid md:grid-cols-[1fr,auto,1fr,auto,1fr]">
+      <CardContent className="p-5 md:grid md:grid-cols-[1fr_auto_1fr_auto_1fr]">
         <div className="">
           {agenda?.organization.logo && (
             <img
@@ -24,7 +27,29 @@ export function BookingForm({ orgSlug, agendaSlug }: BookingFormProps) {
           <p className="text-sm font-medium text-muted-foreground mt-1">
             {agenda?.organization.name}
           </p>
+          <h1 className="text-xl font-semibold mt-2">{agenda?.name}</h1>
+          <p className="text-sm font-medium text-muted-foreground">
+            {agenda?.description}
+          </p>
+
+          <div className="mt-5 flex flex-col gap-y-3">
+            <p className="flex items-center">
+              <Calendar className="size-4 mr-2 text-primary" />
+              <span className="text-sm font-medium text-muted-foreground">
+                {dayjs().format("D MMMM YYYY")}
+              </span>
+            </p>
+
+            <p className="flex items-center">
+              <Clock className="size-4 mr-2 text-primary" />
+              <span className="text-sm font-medium text-muted-foreground">
+                {agenda?.slotDuration} minutos
+              </span>
+            </p>
+          </div>
         </div>
+
+        <Separator orientation="vertical" className="w-px! h-full" />
       </CardContent>
     </Card>
   );
---
+++ src/features/agenda/components/external-link/calendar.tsx
@@ -0,0 +1,6 @@
+import { useCalendar, useLocale } from "react-aria";
+import { createCalendar } from "@internationalized/date";
+
+export function Calendarf() {
+  return <div className="inline-block"></div>;
+}
