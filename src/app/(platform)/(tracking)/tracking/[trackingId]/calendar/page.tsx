b9af60608eac61e9aa1d21ca661468ff844ecd0b
João Gabriel <joaogabriel9633@gmail.com>
2026-03-20 17:51:20 -0300

    create calendar appointment

---
+++ src/app/(platform)/(tracking)/tracking/[trackingId]/calendar/page.tsx
@@ -0,0 +1,9 @@
+import { AppointmentCalendar } from "@/features/trackings/components/appointment-calendar";
+
+export default function Page() {
+  return (
+    <div className="h-full w-full">
+      <AppointmentCalendar />
+    </div>
+  );
+}
